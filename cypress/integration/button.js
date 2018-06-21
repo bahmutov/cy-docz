388b483c264730b6f8527f1f42da7552453b02c6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-06-21 12:41:12 -0400

    testing docs with Cypress

---
+++ cypress/integration/button.js
@@ -0,0 +1,54 @@
+/// <reference types="cypress" />
+//@ts-check
+describe('Button', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/components-button')
+  })
+
+  it('shows on the docz page', () => {
+    cy.get('h2#basic-usage + div > button').should('be.visible')
+  })
+
+  it('shows alert on click', () => {
+    const stub = cy.stub()
+    cy.on('window:alert', stub)
+
+    cy.get('h2#with-onclick-handler + div > button')
+    .scrollIntoView()
+    .click()
+    .then(() => {
+      expect(stub).to.have.been.calledOnce
+    })
+  })
+
+  it('has menu bar', () => {
+    cy.get('nav').should('be.visible')
+      .contains('Components')
+      .click()
+
+    cy.get('nav').contains('a', 'Button').should('be.visible')
+    cy.contains('h1', 'Button').should('be.visible')
+    cy.url().should('contain', 'components-button')
+
+    cy.get('nav').contains('a', 'Alert').should('be.visible').click()
+    cy.contains('h1', 'Alert').should('be.visible')
+    cy.url().should('contain', 'components-alert')
+  })
+
+  it('has menu bar (narrow screen)', () => {
+    cy.viewport('iphone-6')
+
+    // not the best looking page, but the content is (barely) visible
+    cy.get('nav').should('be.visible')
+      .contains('Components')
+      .click()
+
+    cy.get('nav').contains('a', 'Button').should('be.visible')
+    cy.contains('h1', 'Button').should('be.visible')
+    cy.url().should('contain', 'components-button')
+
+    cy.get('nav').contains('a', 'Alert').should('be.visible').click()
+    cy.contains('h1', 'Alert').should('be.visible')
+    cy.url().should('contain', 'components-alert')
+  })
+})
