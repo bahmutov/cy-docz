a56f1c74d936a48ea928ca8e92b9a20b8a333213
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-06-24 11:27:53 -0400

    use menu to get to the button page

--- components/Button-spec.js
+++ components/Button-spec.js
@@ -2,7 +2,12 @@
 //@ts-check
 describe('Button', () => {
   beforeEach(() => {
-    cy.visit('/components-button')
+    cy.visit('/') // visit the page
+    // and go to the components-button page
+    // cannot use direct URL because this static SPA does
+    // not have 404 page redirect back to index!
+    cy.get('nav').contains('Components').click()
+    cy.get('nav').contains('a', 'Button').click()
   })
 
   it('shows on the docz page', () => {
@@ -18,36 +23,5 @@ describe('Button', () => {
     .then(() => {
       expect(stub).to.have.been.calledOnce
     })
-  })
-
-  it('has menu bar', () => {
-    cy.get('nav').should('be.visible')
-      .contains('Components')
-      .click()
-
-    cy.get('nav').contains('a', 'Button').should('be.visible')
-    cy.contains('h1', 'Button').should('be.visible')
-    cy.url().should('contain', 'components-button')
-
-    cy.get('nav').contains('a', 'Alert').should('be.visible').click()
-    cy.contains('h1', 'Alert').should('be.visible')
-    cy.url().should('contain', 'components-alert')
-  })
-
-  it('has menu bar (narrow screen)', () => {
-    cy.viewport('iphone-6')
-
-    // not the best looking page, but the content is (barely) visible
-    cy.get('nav').should('be.visible')
-      .contains('Components')
-      .click()
-
-    cy.get('nav').contains('a', 'Button').should('be.visible')
-    cy.contains('h1', 'Button').should('be.visible')
-    cy.url().should('contain', 'components-button')
-
-    cy.get('nav').contains('a', 'Alert').should('be.visible').click()
-    cy.contains('h1', 'Alert').should('be.visible')
-    cy.url().should('contain', 'components-alert')
   })
 })
